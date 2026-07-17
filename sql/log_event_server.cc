a2c0e63d1fb225a37ef401f013229211e32d860c
ParadoxV5 <paradox.ver5@gmail.com>
2026-07-16 18:11:14 -0600

    MDEV-40365 OOB read on malformed `Format_description_log_event`
    The binary-parsing Format Description Event constructor did not
    validate content length beyond the superclass `is_valid()` call.
    When parsing a malformed FDE, to load the content fields added in
    FDE v4 that are missing in this not-FDE, the parser constructor
    would read from erroneous memory locations beyond the buffer.
    If this did not outright crash the program, this would corrupt the FDE.
    With the Format Description playing a critical role in determining how
    to parse the events to follow, a corrupted FDE would also corrupt (or
    trigger a crash in) the parsing of subsequent non-FDE events as well.
    
    This commit fills in the validation with a FDE-specific guard.
    It adds the FDE constant `ST_POST_HEADER_LEN_OFFSET` to assist
    with comparing to the correct minimum size in the future.
    (Both of these points are designed to merge with the
    superclass’s guard as part of the MDEV-30128 merger).

--- sql/log_event_server.cc
+++ sql/log_event_server.cc
@@ -2372,9 +2372,12 @@ bool Format_description_log_event::write()
     We don't call Start_log_event_v3::write() because this would make 2
     my_b_safe_write().
   */
-  uchar buff[START_V3_HEADER_LEN+1];
-  size_t rec_size= sizeof(buff) + BINLOG_CHECKSUM_ALG_DESC_LEN +
-                   number_of_event_types;
+  uchar buff[ST_POST_HEADER_LEN_OFFSET];
+  const size_t buff_size= DBUG_EVALUATE_IF("truncate_fde_at_common_header_len",
+    ST_COMMON_HEADER_LEN_OFFSET, sizeof(buff));
+  size_t rec_size= buff_size +
+    DBUG_EVALUATE_IF("truncate_fde_at_common_header_len", 0,
+      BINLOG_CHECKSUM_ALG_DESC_LEN + number_of_event_types);
   int2store(buff + ST_BINLOG_VER_OFFSET,binlog_version);
   memcpy((char*) buff + ST_SERVER_VER_OFFSET,server_version,ST_SERVER_VER_LEN);
   if (!dont_set_created)
@@ -2413,10 +2416,11 @@ bool Format_description_log_event::write()
     checksum_alg= BINLOG_CHECKSUM_ALG_CRC32;  // Forcing (V) room to fill anyway
   }
   ret= write_header(rec_size) ||
-       write_data(buff, sizeof(buff)) ||
+    write_data(buff, buff_size) ||
+    DBUG_EVALUATE_IF("truncate_fde_at_common_header_len", false,
        write_data(post_header_len, number_of_event_types) ||
        write_data(&checksum_byte, sizeof(checksum_byte)) ||
-       write_footer();
+       write_footer());
   if (no_checksum)
     checksum_alg= BINLOG_CHECKSUM_ALG_OFF;
   return ret;
